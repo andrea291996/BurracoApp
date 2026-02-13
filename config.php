2748e21805ccf101719bc7589e0af3252683a08e
Andrea <carminati.andra96@gmail.com>
2026-02-13 10:12:30 +0100

    Sistemato configurazione

--- config.php
+++ config.php
@@ -5,5 +5,5 @@ define("TENGINE_CACHE_FOLDER", __DIR__."/cache/mustache");
 define("TENGINE_CACHE_FILE_MODE", 0666);
 define("TENGINE_TEMPLATE_FILE_EXTENSION",".mst");
 
-define("BASE_PATH", "/MVC");
+define("BASE_PATH", "/BurracoApp");
 
